c52f5501777631a55f027b051c4f63de47d81ac7
smazydev <alizafar1437@gmail.com>
2021-06-30 12:59:13 -0400

    added post route for cars controller

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Car } from './interfaces/car.interface';
 import { CarsService } from './cars.service';
+import { CreateCarDTO } from './create-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -9,5 +10,10 @@ export class CarsController {
   @Get()
   async findAll(): Promise<Car[]> {
     return this.carsSerivce.findAll();
+  }
+
+  @Post()
+  async createCar(@Body() createCarDTO: CreateCarDTO): Promise<Car> {
+    return this.carsSerivce.create(createCarDTO);
   }
 }
